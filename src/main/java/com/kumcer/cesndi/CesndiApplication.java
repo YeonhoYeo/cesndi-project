91e4b8f489519dbd80af707cae77a4b0a8d7844c
YeonhoYeo <yoyonho@naver.com>
2019-04-25 17:47:31 +0900

    Initial commit

---
+++ src/main/java/com/kumcer/cesndi/CesndiApplication.java
@@ -0,0 +1,13 @@
+package com.kumcer.cesndi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesndiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CesndiApplication.class, args);
+    }
+
+}
