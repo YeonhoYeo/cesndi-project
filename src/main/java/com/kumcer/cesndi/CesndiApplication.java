b4f65ab11ca1112d0ed7e1e19d8f9b67b4520dbb
YeonhoYeo <yoyonho@naver.com>
2019-05-05 18:49:36 +0900

    # 배포테스트

--- src/main/java/com/kumcer/cesndi/CesndiApplication.java
+++ src/main/java/com/kumcer/cesndi/CesndiApplication.java
@@ -1,15 +1,26 @@
 package com.kumcer.cesndi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing  // JPA Auditing 활성화
 @SpringBootApplication
 public class CesndiApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/cesndi-project/real-application.yml";
+    // + "C:\\develop\\project\\config\\cesndi-project\\real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(CesndiApplication.class, args);
+        new SpringApplicationBuilder(CesndiApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(CesndiApplication.class, args);
+//    }
 
 }
